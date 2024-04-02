b8514a9cc6a222e68a2a145a76ab93317bc3fe4c
Frans Sebastian Villamizar Maldonado <fsvillamizar@uniboyaca.edu.co>
2024-04-02 17:12:44 -0500

    🤢👌

--- TurismoEnSanAndres/src/routes/contact.tsx
+++ TurismoEnSanAndres/src/routes/contact.tsx
@@ -1,7 +1,38 @@
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './db.css';
 
+interface TurismoData {
+    rtn: number;
+    municipio: string;
+    categoria: string;
+}
+
+// Crear una instancia de axios con la configuración de CORS
+const axiosInstance = axios.create({
+    baseURL: 'https://5f74-186-114-123-139.ngrok-free.app/api/turismo/getTurismo',
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+    },
+});
+
 export default function Contact() {
+    const [data, setData] = useState<TurismoData[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await axiosInstance.get<TurismoData[]>('');
+                console.log(result.data);  // Agrega esta línea
+                setData(result.data);
+            } catch (error) {
+                console.error('Error fetching turismo data:', error);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <div className="container">
             <input type="text" placeholder="Ingrese el establecimiento a buscar..." />
@@ -11,23 +42,21 @@ export default function Contact() {
                 <thead>
                     <tr>
                         <th>RNT</th>
-                        <th>Nombre</th>
-                        <th>Dirección</th>
-                        <th>Detalles</th>
+                        <th>Municipio</th>
+                        <th>Categoria</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {/* Aquí puedes mapear tus datos para generar filas de tabla */}
-                    <tr>
-                        <td>123</td>
-                        <td>gagssasssssssssssssssa</td>
-                        <td>Calle Principal 123 asfasf asfasfa</td>
-                        <td>
-                            <button>Ver Detalles</button>
-                        </td>
-                    </tr>
+                    {Array.isArray(data) && data.map(item => (
+                        <tr key={item.rtn}>
+                            <td>{item.rtn ? item.rtn : 'No disponible'}</td>
+                            <td>{item.municipio ? item.municipio : 'No disponible'}</td>
+                            <td>{item.categoria ? item.categoria : 'No disponible'}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
+
         </div>
     );
 }
